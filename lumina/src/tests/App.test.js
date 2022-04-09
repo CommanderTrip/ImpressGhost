3981ed3aa98ff9957e1002784f1aa19a93463025
Trip <cmdrtrip@gmail.com>
2022-04-09 16:05:56 -0500

    File Moved

--- lumina/src/tests/App.test.js
+++ lumina/src/tests/App.test.js
@@ -1,6 +1,6 @@
 /* eslint no-undef: 0 */
 import {render, screen} from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 
 test("renders learn react link", () => {
     render(<App/>);
